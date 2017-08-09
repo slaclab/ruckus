f6ddd55e8612fbd5c0aacd0e7f7e6b05a802686d
Larry Ruckman <ruckman@slac.stanford.edu>
2017-08-09 12:45:22 -0700

    using ::env instead

--- vivado_sources.tcl
+++ vivado_sources.tcl
@@ -11,9 +11,8 @@
 ########################################################
 ## Get variables and Custom Procedures
 ########################################################
-set RUCKUS_DIR $::env(RUCKUS_DIR)
-source -quiet ${RUCKUS_DIR}/vivado_env_var.tcl
-source -quiet ${RUCKUS_DIR}/vivado_proc.tcl
+source -quiet $::env(RUCKUS_DIR)/vivado_env_var.tcl
+source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Check if you have write permission
 CheckWritePermission
