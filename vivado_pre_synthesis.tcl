a644372cb9aef6cdf0aa854da8aa3db165868f37
Larry Ruckman <ruckman@slac.stanford.edu>
2018-10-26 11:07:57 -0700

    added ability to elaborate the design before the main synth_1 build

--- vivado_pre_synthesis.tcl
+++ vivado_pre_synthesis.tcl
@@ -16,5 +16,17 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_env_var.tcl
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
+# Check for optional pre-synthesis elaboration to check for sensitivity list errors
+# Note: This step is not added by default to minimize build time (assuming no code errors)
+if { [info exists ::env(PRE_SYNTH_ELABORATE)] != 1 || $::env(PRE_SYNTH_ELABORATE) == 0 } {
+   set nop 0
+} else {
+   set elab_rc [catch {synth_design -rtl -name rtl_1 -mode out_of_context -rtl_skip_ip -rtl_skip_constraints -no_timing_driven -assert} _ELAB_RESULT]
+   if { ${elab_rc} } {
+      PrintOpenGui ${_ELAB_RESULT}
+      exit -1
+   }   
+}
+
 # Target specific pre_synthesis script
 SourceTclFile ${VIVADO_DIR}/pre_synthesis.tcl
