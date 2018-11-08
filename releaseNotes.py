bd4874896c6ef4a2e8061a5d140eb56ac270b8de
Larry Ruckman <ruckman@slac.stanford.edu>
2018-11-08 09:42:26 -0800

    fixed typo

--- releaseNotes.py
+++ releaseNotes.py
@@ -22,14 +22,15 @@
 #       > git push --tags
 #    - Create release using tag on github.com, use this script to generate notes
 # ----------------------------------------------------------------------------
-# This file is part of the 'SLAC Firmware Standard Library'm. It is subject to 
+# This file is part of the 'SLAC Firmware Standard Library'. It is subject to 
 # the license terms in the LICENSE.txt file found in the top-level directory 
 # of this distribution and at: 
 #    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
-# No part of the 'SLAC Firmware Standard Library'm, including this file, may be 
+# No part of the 'SLAC Firmware Standard Library', including this file, may be 
 # copied, modified, propagated, or distributed except according to the terms 
 # contained in the LICENSE.txt file.
 # ----------------------------------------------------------------------------
+
 ##
 # @file releaseNotes.py
 # Generate release notes for pull requests relative to a tag.
