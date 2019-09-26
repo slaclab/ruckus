5092b93b49a4648f674715a7c97b9c0830ebc752
Larry Ruckman <ruckman@slac.stanford.edu>
2019-09-26 08:14:29 -0700

    bug fix

--- system_vivado.mk
+++ system_vivado.mk
@@ -50,7 +50,7 @@ endif
 
 ifndef RECONFIG_CHECKPOINT
 export RECONFIG_CHECKPOINT = 0
-export RECONFIG_STATIC_HASH = ""
+export RECONFIG_STATIC_HASH = 0
 else
 export RECONFIG_STATIC_FILE = $(notdir $(RECONFIG_CHECKPOINT))
 export RECONFIG_STATIC_HASH = -$(shell echo '$(RECONFIG_STATIC_FILE)' | awk -F'-' '{print $$5}' )
