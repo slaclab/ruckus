9085c37a5d630b7c3039c1c82cf8c80c79760831
Larry Ruckman <ruckman@slac.stanford.edu>
2019-11-15 08:54:31 -0800

    Adding -v2005 to vloganOpt

--- vivado_vcs.tcl
+++ vivado_vcs.tcl
@@ -114,7 +114,7 @@ set simTbOutDir ${OUT_DIR}/${PROJECT}_project.sim/sim_1/behav
 set simTbFileName [get_property top [get_filesets sim_1]]
 
 # Set the compile/elaborate options
-set vloganOpt "-nc -l +v2k -xlrm -kdb +define+SIM_SPEED_UP"
+set vloganOpt "-nc -l +v2k -xlrm -kdb -v2005 +define+SIM_SPEED_UP"
 set vhdlanOpt "-nc -l +v2k -xlrm -kdb"
 set elabOpt "+warn=none -kdb -lca"
 
