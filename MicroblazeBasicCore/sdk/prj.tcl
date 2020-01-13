a21ad50671bde00f310d874624c2b012b70c75c4
Larry Ruckman <ruckman@slac.stanford.edu>
2020-01-13 12:41:46 -0800

    bug fix for MicroblazeBasicCore/sdk & MicroblazeBasicCore/vitis

--- MicroblazeBasicCore/sdk/prj.tcl
+++ MicroblazeBasicCore/sdk/prj.tcl
@@ -14,7 +14,7 @@
 #############################
 ## Get build system variables 
 #############################
-source $::env(RUCKUS_DIR)/vivado_env_var.tcl
+source $::env(RUCKUS_DIR)/vivado/env_var.tcl
 set EmptyApp "Empty Application"
 exec rm -rf ${SDK_PRJ}
 
