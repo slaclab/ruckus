72c3f2953df5e9ca177fb26ad8dbe643e275e5b6
Larry Ruckman <ruckman@slac.stanford.edu>
2020-02-11 09:35:29 -0800

    releaseGen.py depends on the ~/firmware/build/VivadoProject dir

--- system_vivado.mk
+++ system_vivado.mk
@@ -363,7 +363,7 @@ elf :
 #### Release ##################################################
 ###############################################################
 .PHONY : release
-release : 
+release : $(VIVADO_DEPEND)
 	$(call ACTION_HEADER,"Generaring Release")
 	@cd $(OUT_DIR); python3 $(RUCKUS_DIR)/releaseGen.py --project=$(TOP_DIR) --release=$(RELEASE) --push
 
@@ -371,7 +371,7 @@ release :
 #### Release Files ############################################
 ###############################################################
 .PHONY : release_files
-release_files : 
+release_files : $(VIVADO_DEPEND)
 	$(call ACTION_HEADER,"Generaring Release Files")
 	@cd $(OUT_DIR); python3 $(RUCKUS_DIR)/releaseGen.py --project=$(TOP_DIR) --release=$(RELEASE)
 
