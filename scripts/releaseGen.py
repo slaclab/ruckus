9986913a5b691ee5a16ab80e94c77b2aa3a25a97
Ryan Herbst <rherbst@slac.stanford.edu>
2020-03-27 14:00:36 -0700

    New release

---
+++ scripts/releaseGen.py
@@ -0,0 +1,50 @@
+#-----------------------------------------------------------------------------
+# Title      : Release generation
+# ----------------------------------------------------------------------------
+# This file is part of the 'SLAC Firmware Standard Library'. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the 'SLAC Firmware Standard Library', including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+# ----------------------------------------------------------------------------
+import os
+import git                 # GitPython
+from github import Github  # PyGithub
+import re
+import ReleaseNotes
+
+ghRepo = os.environ.get('TRAVIS_REPO_SLUG')
+token  = os.environ.get('GH_REPO_TOKEN')
+newTag = os.environ.get('TRAVIS_TAG')
+
+if ghRepo is None:
+    exit("TRAVIS_REPO_SLUG not in environment.")
+
+if token is None:
+    exit("GH_REPO_TOKEN not in environment.")
+
+if newTag is None:
+    exit("TRAVIS_TAG not in environment.")
+
+# Check tag to make sure it is a proper release: va.b.c
+vpat = re.compile('v\d+\.\d+\.\d+')
+
+if vpat.match(newTag) is None:
+    exit("Not a release version")
+
+# Git server
+gh = Github(token)
+remRepo = gh.get_repo(ghRepo)
+
+# Find previous tag
+oldTag = git.Git('.').describe('--abbrev=0','--tags',newTag + '^')
+
+# Get release notes
+md = ReleaseNotes.getReleaseNotes(locRepo = git.Git('.'), remRepo = ghRepo, oldTag = oldTag, newTag = newTag)
+
+# Create release using tag
+remRel = remRepo.create_git_release(tag=newTag, name=newTag, message=md, draft=False)
+
+print("Success!")
