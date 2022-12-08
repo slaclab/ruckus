0daafd9836d7f4f24b69cdfb9c6e951d02d694f8
Larry Ruckman <ruckman@slac.stanford.edu>
2022-12-08 08:02:19 -0800

    Update sim_management.tcl

--- vivado/proc/sim_management.tcl
+++ vivado/proc/sim_management.tcl
@@ -75,6 +75,6 @@ proc VcsCompleteMessage {dirPath rogueSim} {
       }
    }
    puts "\t\$ ./sim_vcs_mx.sh"
-   puts "\t\$ ./simv -gui=dve& (or $ ./simv -verdi&)"
+   puts "\t\$ ./simv -verdi &"
    puts "********************************************************\n\n"
 }
