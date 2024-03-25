0932924f3e94c2f801c161f4623545a4b5f949c7
Larry Ruckman <ruckman@slac.stanford.edu>
2024-03-25 14:19:19 -0700

    Update download_github_asset.py

--- scripts/download_github_asset.py
+++ scripts/download_github_asset.py
@@ -1,3 +1,15 @@
+# ----------------------------------------------------------------------------
+# Description: Used to download an asset from a tag release on Github
+# ----------------------------------------------------------------------------
+# This file is part of the 'SLAC Firmware Standard Library'. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the 'SLAC Firmware Standard Library', including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+# ----------------------------------------------------------------------------
+
 import os
 import requests
 from github import Github # PyGithub
