47bc886dda208a5378ce1e6fac737991088da584
Larry Ruckman <ruckman@slac.stanford.edu>
2024-11-12 13:55:24 -0800

    adding support to check if old user IP is different to new user IP

--- vivado/proc/ip_management.tcl
+++ vivado/proc/ip_management.tcl
@@ -16,6 +16,7 @@ proc BuildIpCores { } {
 
    # Attempt to upgrade before building IP cores
    upgrade_ip [get_ips]
+   export_ip_user_files -of_objects [get_ips] -no_script -sync -force -quiet
 
    # Check if the target project has IP cores
    if { [get_ips] != "" } {
